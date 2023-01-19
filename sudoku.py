7c26a2420ca51f890cbe6ac47e069e5f5da914ea
isabelplaza <iplaza010@ikasle.ehu.eus>
2023-01-19 10:57:30 +0100

    feat(2): populate the grid with clues
    The displayed window shows a sudoku puzzle with a 9x9 grid and the clues (numbers) for solving the puzzle.

--- sudoku.py
+++ sudoku.py
@@ -9,6 +9,23 @@ WINDOW_SIZE = (550, 550)
 # Set up a background color for the window
 background_color = (250,250,250) # white-grey
 lines_color = (0,0,0) # black
+clues_color = (52, 31, 151) # blue
+
+
+# Populated grid (as an example until developing the sudoku generator module)
+grid = [[7, 8, 0, 4, 0, 0, 1, 2, 0],
+        [6, 0, 0, 0, 7, 5, 0, 0, 9],
+        [0, 0, 0, 6, 0, 1, 0, 7, 8],
+        [0, 0, 7, 0, 4, 0, 2, 6, 0],
+        [0, 0, 1, 0, 5, 0, 9, 3, 0],
+        [9, 0, 4, 0, 6, 0, 0, 0, 5],
+        [0, 7, 0, 3, 0, 0, 0, 1, 2],
+        [1, 2, 0, 0, 0, 7, 4, 0, 0],
+        [0, 4, 9, 2, 0, 6, 0, 0, 7]]
+
+# Copy the grid for later comparison
+grid_original = [[grid[x][y] for y in range(len(grid[0]))] for x in range(len(grid))]
+
 
 def main():
 
@@ -18,6 +35,9 @@ def main():
     # Display the window and fill it with the chosen background color
     window = pygame.display.set_mode(WINDOW_SIZE)
     window.fill(background_color)
+
+    # Add a font for the numbers populating the sudoku grid
+    font = pygame.font.SysFont('Times New Roman', 35)
 
     # Draw the grid (9x9) --> 'for' from 0 to 9.
     for i in range(10):
@@ -35,6 +55,18 @@ def main():
             # Lines in bold
             pygame.draw.line(window, lines_color, (50 + 50*i, 50), (50 + 50*i, 500), 4)
             pygame.draw.line(window, lines_color, (50, 50 + 50*i), (500, 50 + 50*i), 4)
+
+    # Update what is being displayed
+    pygame.display.update()
+
+    # Populate the displayed grid
+    for row in range(len(grid)):
+        for column in range(len(grid[0])):
+            # Check if the value is valid (between 1 and 9)
+            if (1 <= grid[row][column] <= 9):
+                # If valid, write the value in the grid
+                clue = font.render(str(grid[row][column]), True, clues_color)
+                window.blit(clue, ((column+1)*50 + 17, (row+1)*50 + 6))
 
     # Update what is being displayed
     pygame.display.update()
